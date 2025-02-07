35411d2f94a2087d600ea803afd778e76413d946
airoonnee <erwanagesne@gmail.com>
2025-02-07 12:33:12 +0100

    creat team

---
+++ src/Form/TeamType.php
@@ -0,0 +1,34 @@
+<?php 
+
+// src/Form/TeamType.php
+
+namespace App\Form;
+
+use App\Entity\Teams;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'équipe',
+            ])
+            ->add('logo_url', UrlType::class, [
+                'label' => 'URL du logo',
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Teams::class,
+        ]);
+    }
+}
